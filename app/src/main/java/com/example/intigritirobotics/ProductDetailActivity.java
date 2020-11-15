123b220815aa77874be3bf250b65f62ba8593d14
Rounik <rounik1020@gamil.com>
2020-11-15 23:20:57 +0530

    Rating Math Complete

--- app/src/main/java/com/example/intigritirobotics/ProductDetailActivity.java
+++ app/src/main/java/com/example/intigritirobotics/ProductDetailActivity.java
@@ -1,48 +1,35 @@
 package com.example.intigritirobotics;
 
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.Source;
-
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-
-import static com.example.intigritirobotics.MainHomeActivity.firebaseFirestore;
-import static com.example.intigritirobotics.MainHomeActivity.loadingDialog;
+import java.util.Objects;
 
 public class ProductDetailActivity extends AppCompatActivity {
 
     private final List<SlideModel> slideModelList = new ArrayList<>();
-    public static FirebaseFirestore firebaseFirestore;
+    public FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
         firebaseFirestore = FirebaseFirestore.getInstance();
         setContentView(R.layout.activity_product_detail);
         loadProductDetails();
@@ -71,7 +58,7 @@ public class ProductDetailActivity extends AppCompatActivity {
                 .get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-                        DocumentSnapshot doc = task.getResult().getDocuments().get(product_index);
+                        DocumentSnapshot doc = Objects.requireNonNull(task.getResult()).getDocuments().get(product_index);
                         loadDataToProduct(doc);
                     } else {
                         Log.w("Product", "Error getting documents.", task.getException());
@@ -81,8 +68,10 @@ public class ProductDetailActivity extends AppCompatActivity {
     }
 
     private void loadDataToProduct(DocumentSnapshot product) {
-        String in_stock = product.get("in_stock").toString();
-        if(in_stock.equals("false")){
+        String in_stock = Objects.requireNonNull(product.get("in_stock")).toString();
+
+        // In stock
+        if(in_stock.equals("false")) {
             TextView inStockText = findViewById(R.id.inStockText);
             inStockText.setBackgroundColor(Color.RED);
             inStockText.setText("Out of Stock");
@@ -92,23 +81,25 @@ public class ProductDetailActivity extends AppCompatActivity {
             addToCart.setEnabled(false);
         }
 
-        String image_url = product.get("product_pic").toString();
+        // Slider
+        String image_url = Objects.requireNonNull(product.get("product_pic")).toString();
         ImageSlider imageSlider = findViewById(R.id.imgSlider);
 
         slideModelList.add(new SlideModel(image_url, ScaleTypes.FIT));
         imageSlider.setImageList(slideModelList);
 
         TextView briefText = findViewById(R.id.itemBriefDetail);
-        briefText.setText(product.get("product_title").toString());
+        briefText.setText(Objects.requireNonNull(product.get("product_title")).toString());
 
+        // Tabs
         ArrayList<String> arrayList = new ArrayList<>();
         arrayList.add("Some Details");
         arrayList.add("Specs");
         arrayList.add("Other Details");
 
-        String details = product.get("details").toString();
-        String spec = product.get("specs").toString();
-        String other = product.get("other").toString();
+        String details = Objects.requireNonNull(product.get("details")).toString();
+        String spec = Objects.requireNonNull(product.get("specs")).toString();
+        String other = Objects.requireNonNull(product.get("other")).toString();
 
         TabLayout tabLayout = findViewById(R.id.detailTabs);
         ViewPager viewPager = findViewById(R.id.viewPager);
@@ -116,14 +107,49 @@ public class ProductDetailActivity extends AppCompatActivity {
         prepareViewPager(viewPager, arrayList, details, spec, other);
         tabLayout.setupWithViewPager(viewPager);
 
+        // Set Price
         TextView price = findViewById(R.id.PriceText);
-        String priceText = "Rs."+product.get("product_price").toString()+"/-";
+        String priceText = "Rs."+ Objects.requireNonNull(product.get("product_price")).toString()+"/-";
         price.setText(priceText);
+
+        TextView MRP = findViewById(R.id.cut_price);
+        String mrpText = "Rs. " + Objects.requireNonNull(product.get("MRP")).toString() + "/-";
+        MRP.setText(mrpText);
+        MRP.setPaintFlags(MRP.getPaintFlags()|Paint.STRIKE_THRU_TEXT_FLAG);
+
         // Set Ratings
         TextView avgRatingText = findViewById(R.id.averageRatingText);
-        avgRatingText.setText(product.get("product_rating").toString());
+        avgRatingText.setText(getAvg(product));
 
+        String total = Objects.requireNonNull(product.get("total_ratings")).toString();
         TextView totalRatings = findViewById(R.id.number_of_rating_text);
-        totalRatings.setText(product.get("total_ratings").toString());
+        totalRatings.setText(total);
+
+        setProgressInRacingBars(findViewById(R.id.stars1), 1, product);
+        setProgressInRacingBars(findViewById(R.id.stars2), 2, product);
+        setProgressInRacingBars(findViewById(R.id.stars3), 3, product);
+        setProgressInRacingBars(findViewById(R.id.stars4), 4, product);
+        setProgressInRacingBars(findViewById(R.id.stars5), 5, product);
+
     }
+
+    private String getAvg(DocumentSnapshot product) {
+        float sum = 0, total = 0, temp;
+        for(int i=1;i<=5;i++) {
+            temp = Integer.parseInt(Objects.requireNonNull(product.get(i + "_stars")).toString());
+            sum += i*temp;
+            total += temp;
+        }
+        String average = ""+sum/total;
+        if(average.length()>3) average = average.substring(0,3);
+        return  average;
+    }
+
+    void setProgressInRacingBars(ProgressBar progressBar, int starNo, DocumentSnapshot product) {
+        int stars = Integer.parseInt(Objects.requireNonNull(product.get(starNo + "_stars")).toString());
+        int total = Integer.parseInt(Objects.requireNonNull(product.get("total_ratings")).toString());
+        progressBar.setMax(total);
+        progressBar.setProgress(stars);
+    }
+
 }

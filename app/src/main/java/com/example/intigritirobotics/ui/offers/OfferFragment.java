25490dba148febea099e2a675758067c71037bc6
lenchoss5692 <lenchoss20@gmail.com>
2020-12-03 23:47:56 +0530

    OfferFragment completed

--- app/src/main/java/com/example/intigritirobotics/ui/offers/OfferFragment.java
+++ app/src/main/java/com/example/intigritirobotics/ui/offers/OfferFragment.java
@@ -4,32 +4,69 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.example.intigritirobotics.CategoryAdapter;
+import com.example.intigritirobotics.CategoryModel;
 import com.example.intigritirobotics.R;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.intigritirobotics.MainHomeActivity.firebaseFirestore;
+import static com.example.intigritirobotics.MainHomeActivity.loadingDialog;
 
 public class OfferFragment extends Fragment {
 
-    private OfferViewModel offerViewModel;
+    private List<OfferViewModel> offerslist = new ArrayList<>();
+    private RecyclerView offerRecyclerView;
+    private LinearLayoutManager offerLayoutManager;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        offerViewModel =
-                new ViewModelProvider(this).get(OfferViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        offerViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
+        View view = inflater.inflate(R.layout.fragment_offers, container, false);
+        offerRecyclerView = view.findViewById(R.id.offer_recyclerview);
+        offerLayoutManager  = new LinearLayoutManager(getContext());
+
+        loadOffer();
+        return view;
+    }
+
+    private  void loadOffer()
+    {
+        firebaseFirestore.collection("OFFERS").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                offerslist.clear();
+                for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                    offerslist.add(new OfferViewModel(
+
+                            documentSnapshot.get("Banner").toString(),
+                            documentSnapshot.get("Id").toString(),
+                            (Boolean) documentSnapshot.get("Expired")));
+                    Toast.makeText(getContext(), documentSnapshot.get("Banner").toString(), Toast.LENGTH_SHORT).show();
+
+                }
+                offerLayoutManager.setOrientation(RecyclerView.VERTICAL);
+                offerRecyclerView.setLayoutManager(offerLayoutManager);
+                OfferAdapter adapter = new OfferAdapter(offerslist);
+                offerRecyclerView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+
+            }
+            else {
+                loadingDialog.dismiss();
+                String error = task.getException().getMessage();
+                Toast.makeText(getContext(), error, Toast.LENGTH_SHORT).show();
             }
         });
-        return root;
     }
 }

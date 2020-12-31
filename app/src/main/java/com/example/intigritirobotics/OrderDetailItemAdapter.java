9e03ee6053423e34edb8a0db2c9923e898e5fa98
Rounik <rounik1020@gamil.com>
2020-12-31 17:44:41 +0530

    Rating System Updated

--- app/src/main/java/com/example/intigritirobotics/OrderDetailItemAdapter.java
+++ app/src/main/java/com/example/intigritirobotics/OrderDetailItemAdapter.java
@@ -11,17 +11,25 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+
+import static com.example.intigritirobotics.MainHomeActivity.currentUserUId;
+import static com.example.intigritirobotics.MainHomeActivity.firebaseFirestore;
 
 public class OrderDetailItemAdapter extends RecyclerView.Adapter<OrderDetailItemAdapter.ODIViewHolder> {
 
     private final List<OrderDetailItemsModel>OrderDetailItemAdapterList;
+    public int is_app_starting = 0;
 
     public OrderDetailItemAdapter(List<OrderDetailItemsModel> orderDetailItemAdapterList) {
         OrderDetailItemAdapterList = orderDetailItemAdapterList;
+        is_app_starting++;
     }
 
     @NonNull
@@ -29,6 +37,7 @@ public class OrderDetailItemAdapter extends RecyclerView.Adapter<OrderDetailItem
     public ODIViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.ordered_items_layout,viewGroup,false);
         return new ODIViewHolder(view);
+
     }
 
     @Override
@@ -52,6 +61,7 @@ public class OrderDetailItemAdapter extends RecyclerView.Adapter<OrderDetailItem
         private final TextView qty;
         private final TextView price;
         private final RatingBar ProductRating;
+        private int is_app_starting = 0;
 
         public ODIViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -82,13 +92,64 @@ public class OrderDetailItemAdapter extends RecyclerView.Adapter<OrderDetailItem
                    String priceText = "Rs."+Price+"/-";
                    price.setText(priceText);
                    ProductRating.setRating(Float.parseFloat(Rating));
+                   is_app_starting++;
                }
+            });
+
+            ProductRating.setOnRatingBarChangeListener((ratingBar, v, b) -> {
+
+                is_app_starting++;
+
+                Map<String, String> userRatingMap = new HashMap<>();
+                userRatingMap.put("Rating", "" + v);
+
+                Map<String, Object> productUpdateMap = new HashMap<>();
+
+                DocumentReference productRef = firebaseFirestore.document("PRODUCTS/" + ID);
+
+                productRef.get().addOnCompleteListener(task -> {
+                    if (task.isSuccessful() && is_app_starting > 1) {
+                        DocumentSnapshot prodSnap = task.getResult();
+                        assert prodSnap != null;
+                        String s = prodSnap.get((v + "").substring(0, 1) + "_star").toString();
+
+                        int x = s==null ? 0 : Integer.parseInt(s);
+
+                        firebaseFirestore
+                                .document("USERS/"+currentUserUId+"/My Ratings/"+ID)
+                                .get()
+                                .addOnCompleteListener(task1 -> {
+                                    if(task1.isSuccessful()) {
+
+                                        if(Objects.requireNonNull(task1.getResult()).exists()) {
+                                            String prevRating = task1.getResult().get("Rating").toString();
+                                            int pre_num = Integer.parseInt(prodSnap.get(prevRating.substring(0,1)+"_star").toString());
+
+                                            Log.d("Prev num is: ",""+pre_num);
+
+                                            productUpdateMap.put(prevRating.substring(0,1)+"_star", (pre_num-1)+"");
+                                        }
+
+                                        firebaseFirestore.document("USERS/" + currentUserUId + "/My Ratings/" + ID).set(userRatingMap);
+                                        productUpdateMap.put((int)v+"_star", x+1+"");
+
+                                        Log.d("Map Me kya hai",productUpdateMap.keySet().toString());
+                                        Log.d("Map Values", productUpdateMap.values().toString());
+
+                                        productRef.update(productUpdateMap);
+                                    }
+                                });
+
+                    }
+                });
+
             });
 
             itemView.setOnClickListener(v -> {
                 Intent intent = new Intent( itemView.getContext(),ProductDetailActivity.class);
                 intent.putExtra("ID", ID);
-                intent.putExtra("Price",""+price);
+                intent.putExtra("Price",""+price.getText().subSequence(3,price.getText().length()-2));
+                intent.putExtra("Title", title.getText());
                 itemView.getContext().startActivity(intent);
             });
 
